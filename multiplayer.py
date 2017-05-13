7c364f2e61dcea6cff34977327a00962f8f4bb2f
eadgyo <eadgyo@gmail.com>
2017-05-13 18:44:42 +0200

    [ADD] Multiplayer skeleton

---
+++ multiplayer.py
@@ -0,0 +1,16 @@
+class multiplayer:
+	def __init__(self):
+		pass
+	
+	def multiplayer(self):
+		pass
+	
+	
+	def sendObject(self):
+		pass
+	
+	def handleUserAdded(self):
+		pass
+
+	def handleObjectAdded(self):
+		pass
