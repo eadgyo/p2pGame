0b0b02240603bec6cca304c1e49a4550f2652999
Tugdwal <tudal.le.bot@gmail.com>
2017-05-18 16:13:51 +0200

    [UPDATE] main.py

--- main.py
+++ main.py
@@ -2,31 +2,15 @@
 
 from game import *
 
-"""
-def main():
-    port = int(input("Port number ? "))
-    game = server.Server(port)
-
-    b = input("Start new game ? ")
-    
-    if b == "y":
-        game.new()
-    else:
-        ip = input("Server hostname ? ")
-        if ip == "":
-            ip = game.host
-        port = int(input("Server port number ? "))
-        game.connect(ip, port)
-"""
-
 def main():
     port = int(input("Port number ? "))
     game = Game(port)
 
     b = input("Start new game ? ")
-    
+
     if b == 'y':
-        game.new()
+        n = int(input("Number of players ? "))
+        game.new(n)
     else:
         ip = input("Server hostname ? ")
         if ip == '':
@@ -35,3 +19,4 @@ def main():
         game.connect(ip, port)
 if __name__ == "__main__":
     main()
+
